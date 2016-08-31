8c9e841951a57902ca07b70527ccb7f1a1268a7e
Suszyński Krzysztof <krzysztof.suszynski@coi.gov.pl>
2016-08-31 17:52:47 +0200

    Basic structure

---
+++ build.sbt
@@ -0,0 +1,6 @@
+lazy val root = (project in file(".")).
+  settings(
+    name := "rh-harvester",
+    version := "0.1.0-SNAPSHOT",
+    scalaVersion := "2.11.8"
+  )
---
+++ src/main/java/pl/wavesoftware/hacking/rhharvester/Main.java
@@ -0,0 +1,8 @@
+package pl.wavesoftware.hacking.rhharvester;
+
+public final class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello!");
+  }
+}
+
